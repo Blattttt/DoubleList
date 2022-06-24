824b501e7c74cba145c31d688af61f3e38b617a3
Blat <ilovemydogdeck@mail.ru>
2022-06-24 10:54:29 +0300

    Create repository

---
+++ src/sample/MethodTest.java
@@ -0,0 +1,13 @@
+package sample;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MethodTest extends Method{
+
+    @Test
+    void upField() {
+        Method method = new Method();
+        String actual = method.UpField("1", "2");
+        assertEquals("2", actual);
+    }}
+
